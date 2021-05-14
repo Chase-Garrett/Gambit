85d5c1c330c01ccc1adee05633fcecd34e401bf9
Chase-Garrett <chasegarrett@tutanota.com>
2021-05-14 01:34:32 -0500

    added DiceRoll module

--- dependencies/DiceRoll.js
+++ dependencies/DiceRoll.js
@@ -1,8 +1,16 @@
+'use strict';
+
 // Module for handling dice rolling
 class DiceRoll{
+    // Constructor takes in number of dice to roll
+    // and the number of sides on the dice
     constructor(numDice, diceSides){
         this.numDice = numDice;
         this.diceSides = diceSides;
+    }
+
+    roll(){
+        return (this.numDice * Math.floor(Math.random() * (this.diceSides) + 1));
     }
 }
 
