f6f61a13c1f7175d0fba5d57bfb9172a79e120db
Chase-Garrett <chasegarrett@tutanota.com>
2021-05-20 15:12:50 -0500

    rebuild message listener to more accurately take commands

--- src/Gambit.js
+++ src/Gambit.js
@@ -23,54 +23,80 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+function doHelp(message){
+    message.channel.send('```\n-roll xdy replace x with number of dice to roll and y with sides on the dice' +
+    '\n```');
+}
+
+function doRoll(args, message){
+    let command;
+
+    try {
+        command = args[1].split("d");
+    } catch (err) {
+        console.error("ERROR: invalid arguments");
+        return;
+    }
+
+    let numDice;
+    let numSides;
+
+    if (command[0] != "") {
+        numDice = command[0];
+        numSides = command[1];
+    } else if (command[0] == "") {
+        numDice = 1;
+        numSides = command[1];
+    } else {
+        console.error("ERROR: invalid arguments");
+        return;
+    }
+
+    // If statement that locks in the number of rolls for a given command
+    if(parseInt(numDice) <= 100){
+        let dice = new DiceRoll(parseInt(numDice), parseInt(numSides));
+
+        // switch statement to determine allowable dice
+        switch(parseInt(numSides)){
+            case 4:
+            case 6:
+            case 8:
+            case 10:
+            case 12:
+            case 20:
+                message.channel.send(dice.roll())
+                break;
+            default:
+                console.error("ERROR: invalid dice type");
+                return;
+        }
+    } else{
+        message.channel.send('You are trying to roll to many dice!');
+    }
+}
+
 //Create a listener for messages
 client.on('message', message => {
-    // If the message is "-help" or "-h"
-    if(message.content === `${prefix}help` || message.content === `${prefix}h`){
-        // Print list of commands
-        message.channel.send('```\n-roll xdy replace x with number of dice to roll and y with sides on the dice' +
-        '\n```');
-    }else if(message.content.startsWith(`${prefix}roll`)) { // takes -roll command and returns a value based on the dice rolled
-        let input = message.content;
-        let args = input;
-        let command;
-        try{
-            args = input.slice(prefix.length).trim().split(/ +/);
-            console.log('args try ' + args)
-            command = args[1].split('d');
-            console.log('command try ' + command)
-        }
-        catch(err){
-            args = input.slice(prefix.length).slice('roll').trim();
-            console.log('args catch ' + args)
-            command = args[1].split('d');
-            console.log('command catch ' + command)
-        }
-        //let command = args[1].split('d');
-        console.log('before if statement');
-        console.log(args);
+    if(message.content[0] == prefix){
+        let command = message.content.slice(1).trim().split(/ +/);
         console.log(command);
 
-        //If statement that locks in the number of rolls for a given command
-        if(parseInt(command[0]) <= 100){
-            let dice = new DiceRoll(parseInt(command[0]), parseInt(command[1]));
-            console.log('in if statement');
-            console.log(args);
-            console.log(command);
-
-            // switch statement to determine allowable dice
-            switch(parseInt(command[1])){
-                case 4:
-                case 6:
-                case 8:
-                case 10:
-                case 12:
-                case 20:
-                    message.channel.send(dice.roll())
-                    break;
-            }
-        } else{
-            message.channel.send('You are trying to roll to many dice!');
+        // Switch statement to check input against available commands
+        // prevents unnecessary conditional checking in cmd functions
+        switch(command[0]){
+            case "help":
+            case "h":
+                doHelp(message);
+                break;
+            case "coin":
+            case "cointoss":
+                doCoinToss(message);
+                break;
+            case "roll":
+                doRoll(command.slice(0), message);
+                break;
+            default:
+                console.error(`ERROR: command ${command[0]} no supported.`);
         }
     }
 });
